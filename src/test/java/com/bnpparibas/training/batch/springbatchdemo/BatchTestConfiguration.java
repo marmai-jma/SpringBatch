65cf400d3b854f68051f94bd155d4e0c096bec24
marmai-jma <johane.marmai@gmail.com>
2019-11-18 17:07:01 +0100

    Version avec les tests mais qui plante

---
+++ src/test/java/com/bnpparibas/training/batch/springbatchdemo/BatchTestConfiguration.java
@@ -0,0 +1,33 @@
+package com.bnpparibas.training.batch.springbatchdemo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@SpringBootConfiguration
+@EnableBatchProcessing
+public class BatchTestConfiguration {
+    @Bean
+    public JobLauncherTestUtils jobLauncherTestUtils(){
+        return new JobLauncherTestUtils();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(final DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema-all.sql")
+                .build();
+    }
+}
---
+++ src/test/java/com/bnpparibas/training/batch/springbatchdemo/ImportStepConfigTest.java
@@ -0,0 +1,49 @@
+package com.bnpparibas.training.batch.springbatchdemo;
+
+import com.bnpparibas.training.batch.springbatchdemo.config.ImportJobConfig;
+import com.bnpparibas.training.batch.springbatchdemo.config.JobCompletionNotificationListener;
+import com.bnpparibas.training.batch.springbatchdemo.config.MaClasseMetier;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.*;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.jdbc.JdbcTestUtils.deleteFromTables;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = {BatchTestConfiguration.class, ImportJobConfig.class,
+        JobCompletionNotificationListener.class, MaClasseMetier.class})
+public class ImportStepConfigTest {
+    @Autowired
+    private JobLauncherTestUtils testUtils;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void deleteBooks(){
+        deleteFromTables(jdbcTemplate,"book");
+    }
+
+    @Test
+    public void importJob() throws Exception {
+        //Given
+        final JobParameters jobParameters = new JobParametersBuilder(testUtils.getUniqueJobParameters())
+                .addString("input-file","src/main.ressources/sample-data.csv")
+                .toJobParameters();
+        //when
+        final JobExecution jobExec = testUtils.launchJob(jobParameters);
+        // then
+        assertThat(jobExec.getStatus(), equalTo(BatchStatus.COMPLETED));
+    }
+}
